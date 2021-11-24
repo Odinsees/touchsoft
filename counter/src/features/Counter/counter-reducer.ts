44aad4cbbad25480ae3b3b53db305395239a9a5a
Odinsees <pavel.o.lebedev@gmail.com>
2021-11-24 22:53:16 +0300

    create store, reducer, refactor file directory

--- counter/src/features/Counter/counter-reducer.ts
+++ counter/src/features/Counter/counter-reducer.ts
@@ -1,19 +1,41 @@
-
 type InitialStateType = {
-
+    value: number
 }
 
-type ActionType = {}
+type ActionType =
+    |ReturnType<typeof resetCount>
+    |ReturnType<typeof incrementCount>
+    |ReturnType<typeof decrementCount>
 
-const initialState={
-
+const initialState = {
+    value: 0
 }
 
-export const counterReducer = (state:InitialStateType = initialState, action:ActionType):InitialStateType => {
-    // @ts-ignore
-    switch (action.type){
+export const counterReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
+    switch (action.type) {
+        case "RESET-COUNT":
+            return {
+                ...state,
+                value: 0
+            }
+        case "INCREMENT-COUNT":
+            return {
+                ...state,
+                value: state.value + 1
+            }
+        case "DECREMENT-COUNT":
+            return {
+                ...state,
+                value: state.value - 1
+            }
         default:
             return state
     }
 }
+
+
+export const resetCount = () => ({type: "RESET-COUNT"} as const)
+export const incrementCount = () => ({type: "INCREMENT-COUNT"} as const)
+export const decrementCount = () => ({type: "DECREMENT-COUNT"} as const)
+
 
